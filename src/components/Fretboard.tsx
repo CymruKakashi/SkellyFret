876a39db8421afd16133bfdba922feca5f35147f
Ashley Workman <cymrukakashi@gmail.com>
2022-03-15 18:54:29 +0000

    Working controls

--- src/components/Fretboard.tsx
+++ src/components/Fretboard.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Col, Container, InputGroup, Row } from "react-bootstrap";
+import { Col, Container, Dropdown, InputGroup, Row } from "react-bootstrap";
 import Board from "./Board";
 import "./Fretboard.scss";
 
@@ -7,9 +7,27 @@ export default function Fretboard() {
 
     const [boardSize, setBoardSize] = useState(13);
     const [strings, setStrings] = useState(6);
-    const [tuning, setTuning] = useState(2);
+    const [tuning, setTuning] = useState(0);
     const [instrument, setInstrument] = useState(0);
     const [naturalOnly, setNaturalOnly] = useState(false);
+    const [isBass, setIsBass] = useState(false);
+
+    function switchToGuitar()
+    {
+        setInstrument(0);
+        if(strings < 6)
+        {
+            setStrings(6);
+        }
+        setIsBass(false);
+    }
+
+    function switchToBass()
+    {
+        setInstrument(1);
+        setStrings(4);
+        setIsBass(true);
+    }
 
     return (
     <>
@@ -18,8 +36,39 @@ export default function Fretboard() {
             <Container fluid>
                 <Row>
                     <Col>
-                        <span>Natural Notes Only</span>
-                        <input className="ml-1" type='checkbox'></input>
+                    { !isBass &&
+                        <Dropdown className="mb-2">
+                            <Dropdown.Toggle variant="secondary" id="dropdown-basic">
+                                Strings
+                            </Dropdown.Toggle>
+                            <Dropdown.Menu>
+                                <Dropdown.Item onClick={() => setStrings(6)}>Six</Dropdown.Item>
+                                <Dropdown.Item onClick={() => setStrings(7)}>Seven</Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    }
+                    </Col>
+                    <Col>
+                        <Dropdown className="mb-2">
+                            <Dropdown.Toggle variant="secondary" id="dropdown-basic">
+                                Tuning
+                            </Dropdown.Toggle>
+                            <Dropdown.Menu>
+                                <Dropdown.Item onClick={() => setTuning(0)}>Standard</Dropdown.Item>
+                                <Dropdown.Item onClick={() => setTuning(2)}>Whole Step Down</Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    </Col>
+                    <Col>
+                        <Dropdown className="mb-2">
+                            <Dropdown.Toggle variant="secondary" id="dropdown-basic">
+                                Guitar/Bass
+                            </Dropdown.Toggle>
+                            <Dropdown.Menu>
+                                <Dropdown.Item onClick={switchToGuitar}>Guitar</Dropdown.Item>
+                                <Dropdown.Item onClick={switchToBass}>Bass</Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
                     </Col>
                 </Row>
             </Container>
@@ -29,6 +78,14 @@ export default function Fretboard() {
                 tuning={tuning}
                 instrument={instrument}
                 naturalOnly={naturalOnly}/>
+            <Container fluid>
+                <Row>
+                    <Col>
+                        <span>Natural Notes Only </span>
+                        <input type='checkbox' onClick={() => setNaturalOnly(!naturalOnly)}></input>
+                    </Col>
+                </Row>
+            </Container>
         </Container>
     </>
     );
